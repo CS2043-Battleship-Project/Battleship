6e904870f2535a133ce4a60fb582f9f13e31a3fd
Silvernite12 <adwhampton_spark@yahoo.ca>
2017-03-31 12:19:32 -0300

    Added Client

---
+++ src/Client.java
@@ -0,0 +1,102 @@
+import java.io.*;
+import java.net.*;
+
+public class Client
+{
+  private String host;
+  private int port;
+  private ServerSocket socket;
+  private Socket s;
+  private PrintWriter out;
+  private BufferedReader in;
+  private BufferedReader stdln;
+
+
+  public Client(String host, int port)
+  {
+     this.host = host;
+     this.port = port;
+      s = null;
+
+    try
+    {
+        s = new Socket(host, port);
+    }
+    catch (UnknownHostException e)
+    {
+      System.err.println("Unknown host: " + host);
+      System.exit(-1);
+    }
+    catch (IOException e)
+    {
+      System.err.println("Unable to get I/O connection to: "
+                        + host + " on port: " + port);
+      System.exit(-1);
+    }
+
+
+  }
+  public void openStream()
+  {
+    try
+    {
+      out = new PrintWriter(s.getOutputStream(), true);
+      in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+      stdln = new BufferedReader(new InputStreamReader(System.in));
+    }
+    catch(IOException e)
+    {
+        System.out.println("Problem reading or writing:" + e.getMessage());
+    }
+  }
+  public void messageToServer()
+  {
+      boolean done = false;
+      try{
+      while(!done )
+      {
+        String line = in.readLine();
+        if(line == null)
+        {
+          done = true;
+        }
+        else
+        {
+          out.println(line);
+        }
+      }
+    }
+    catch(IOException e)
+    {
+      System.out.println("Problem reading or writing:" + e.getMessage());
+    }
+  }
+  public void closeStream()
+  {
+
+    try
+    {
+        in.close();
+        out.close();
+        stdln.close();
+        s.close();
+
+    }
+    catch(IOException e)
+    {
+      System.out.println("Problem with closing the reader, writer or socket");
+    }
+
+
+  }
+
+
+  public static void main(String[] args)
+  {
+    Client client =new Client("localhost", 31415);
+    client.openStream();
+    client.messageToServer();
+    client.closeStream();
+
+  }
+}
